3f984c42a2c63df2bc1d6f55a1fe13138565f8f9
sabrinalaura0210 <sabrinalaura025@gmail.com>
2026-08-21 19:19:54 +0700

    Memperbaiki file migration film dan merefresh database

--- laravel/database/migrations/2026_08_21_085712_create_film_table.php
+++ laravel/database/migrations/2026_08_21_085712_create_film_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('film', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->string('judul', 45);
             $table->text('ringkasan');
             $table->integer('tahun');
