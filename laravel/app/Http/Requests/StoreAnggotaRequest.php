4e2490eabca7ea02f8267b2147582fa14d01f179
Bintang-bima <bintangbima147@gmail.com>
2026-09-25 13:23:35 +0700

    Anggota/request_model

---
+++ laravel/app/Http/Requests/StoreAnggotaRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAnggotaRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'nama' => 'required',
+            'nis' => 'required',
+            'alamat' => 'required',
+            'no_telp' => 'required',
+        ];
+    }
+}
